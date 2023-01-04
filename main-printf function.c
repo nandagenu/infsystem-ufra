9e72df9baa279b8189ec21db6918c1a3d1ba7c92
Fernanda Genú <nandafmgenu@gmail.com>
2023-01-04 10:10:41 -0300

    Técnicas de Programação I

---
+++ main-printf function.c
@@ -0,0 +1,14 @@
+// estrutura básica
+
+// inclusão de bibliotecas
+#include <stdio.h>
+
+// declaração de variáveis globais (inicio e fim)
+int main(void) {
+
+// declaração de variáveis locais
+printf("Teste"/n);
+
+// processamento de dados
+	return 0;
+}
