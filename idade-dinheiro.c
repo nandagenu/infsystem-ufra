d3487e856289c0d1c5df5a55c7a794bc758b33cd
Fernanda Genú <nandafmgenu@gmail.com>
2023-01-04 11:42:56 -0300

    Técnicas de Programação I

---
+++ idade-dinheiro.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+	int idade;
+	float dinheiro;
+	idade = 30;
+	dinheiro = 3.6;
+
+	printf("sua idade é %d /n", idade);
+	printf("na minha carteira tem %.2f", dinheiro);
+	return 0;
+}
