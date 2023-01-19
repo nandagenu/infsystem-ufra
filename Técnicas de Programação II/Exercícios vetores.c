b20c2548d2c817bfa2bf3d27e0ac3ca83d2ed35c
Fernanda Genú <nandafmgenu@gmail.com>
2023-01-19 11:06:48 -0300

    Create Exercícios vetores.c

---
+++ Técnicas de Programação II/Exercícios vetores.c
@@ -0,0 +1,46 @@
+// Crie um vetor de 10 posições do tipo inteiro e faça um programa que permita o preenchimento de todo o vetor com os valores digitados pelo usuário. Após a leitura, mostre os valores lidos.
+
+#include <stdio.h>
+
+int main() {
+
+  int idade[10], i;
+
+  printf("Vamos ler a idade de 10 alunos em uma sala.\n");
+  
+  for(i=0;i<10;i++) {
+    printf("Digite a idade do aluno %d: ", i+1);
+    scanf("%d", &idade[i]);
+  }
+  
+  for(i=0;i<10;i++) {
+    printf("A idade do aluno %d é %d.\n", i+1, idade[i]);
+  }
+  
+  return 0;
+}
+
+//-------------------------------------------------------------------------
+
+// Crie um vetor de 10 posições do tipo inteiro e faça um programa que permita o preenchimento de todo o vetor com os valores digitados pelo usuário, e faça também a soma de todos os seus valores e mostre o resultado.
+
+#include <stdio.h>
+
+int main() {
+  
+  int idade[10], soma_idade, i;
+
+  printf("Vamos ler a idade de 10 alunos em uma sala.\n");
+
+  soma_idade = 0;
+  
+  for(i=0;i<10;i++) {
+    printf("Digite a idade do aluno %d: ", i+1);
+    scanf("%d", &idade[i]);
+    soma_idade = soma_idade + idade[i];
+  }
+
+  printf("A soma de todas as idades será %d", soma_idade);
+  
+  return 0;
+}
